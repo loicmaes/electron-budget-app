1de76f9997418914a8faf0a466d2339d982c1c50
loicmaes <97891718+loicmaes@users.noreply.github.com>
2023-03-26 20:42:41 +0200

    :fire: Remove preload method

---
+++ src/Preload.js
@@ -0,0 +1,12 @@
+// Load versions
+/*window.addEventListener("DOMContentLoaded", () => {
+    const replaceText = (select, text) => {
+        const el = document.querySelector(select)
+        if (!el) return
+        el.innerText = text
+    }
+
+    for (const type of ['chrome', 'node', 'electron']) {
+        replaceText(`[data-info="${type}"]`, process.versions[type])
+    }
+})*/
